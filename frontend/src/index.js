7e84f7442dd8f2081538fb521ef4d0593b7ff964
lexconger <114255759+lexconger@users.noreply.github.com>
2023-06-12 13:22:37 -0700

    Add react-router and page routing

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,8 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import Home from './routes/Home';
+import Contact from './routes/Contact';
+
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom"
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />
+  },
+  {
+    path: "/contact",
+    element: <Contact />
+  },
+  
+])
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -11,13 +28,8 @@ root.render(
     <link rel="preconnect" href="https://fonts.googleapis.com"></link>
     <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true"></link>
     <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@100&display=swap" rel="stylesheet"></link>
-    {/* <React.StrictMode> */}
-      <App />
-    {/* </React.StrictMode> */}
+    <React.StrictMode>
+      <RouterProvider router={router} />
+    </React.StrictMode>
   </>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
